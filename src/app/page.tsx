386be3a57d05e92392de64bf71a0fb3f5bfaaddd
0xdevabir <abir573912@gmail.com>
2025-04-22 18:35:45 +0600

    mad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,14 +38,14 @@ export default function Home() {
     <Loader/>
     <div className={oswald.className}>
       <div className="flex flex-col justify-center mt-[20vh]">
-        <p className='text-center text-[5vw] md:text-[1.8vw] font-[300]' data-aos='fade-in' data-aos-delay="1600">👋🏻, my name is Abir and I am a Freelancer</p>
+        <div data-aos='fade-in' data-aos-delay="1600"><p className='text-center text-[5vw] md:text-[1.8vw] font-[300]'>👋🏻, my name is Abir and I am a Freelancer</p></div>
         <div className="leading-[12vh] md:leading-[18vh]">
-          <p className='text-center text-[15vw] md:text-[10vw] font-[700] cursor-pointer text-stroke1 z-20' data-aos='fade-right' data-aos-delay="1600">Webdeveloper</p>
-          <p className='text-center text-[15vw] md:text-[10vw] font-[700] text-stroke2 cursor-pointer z-10' data-aos='fade-left' data-aos-delay="1600">& <span className='sm:hidden'> <br className='sm:hidde' /> </span> App Developer</p>
+        <div data-aos='fade-right' data-aos-delay="1600"><p className='text-center text-[15vw] md:text-[10vw] font-[700] cursor-pointer text-stroke1 z-20'>Webdeveloper</p></div>
+        <div data-aos='fade-left' data-aos-delay="1600"><p className='text-center text-[15vw] md:text-[10vw] font-[700] text-stroke2 cursor-pointer z-10'>& <span className='sm:hidden'> <br className='sm:hidde' /> </span> App Developer</p></div>
         </div>
         <div className="flex justify-center gap-[5vw] sm:gap-[1vw] mt-[5vh] md:mt-[12vh]">
-          <button className='text-[16px] sm:text-[24px] bg-[#1b1b1b] text-white px-[20px] sm:px-[32px] py-[8px] sm:py-[12px] rounded-md cursor-pointer hero-btn-1' data-aos='fade-up' data-aos-delay="2200">Resume</button>
-          <button className='text-[16px] sm:text-[24px] bg-[white] text-[#1b1b1b] px-[20px] sm:px-[32px] py-[8px] sm:py-[12px] rounded-md cursor-pointer hero-btn-2' data-aos='fade-up' data-aos-delay="2500">Contact</button>
+          <div data-aos='fade-up' data-aos-delay="2200"><button className='text-[16px] sm:text-[24px] bg-[#1b1b1b] text-white px-[20px] sm:px-[32px] hover:px-[50px] py-[8px] sm:py-[12px] rounded-md cursor-pointer hero-btn-1'>Resume</button></div>
+          <div data-aos='fade-up' data-aos-delay="2500"><button className='text-[16px] sm:text-[24px] bg-[white] text-[#1b1b1b] px-[20px] sm:px-[32px] hover:px-[50px] py-[8px] sm:py-[12px] rounded-md cursor-pointer hero-btn-2'>Contact</button></div>
         </div>
       </div>
     </div>
